1879a776ddb4a7002237a1a0ac9230b7b338ddef
Shivtej Pisal <shivtejpisal07@gmail.com>
2025-03-07 15:44:55 +0530

    User Login & Token Generation

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -29,3 +29,31 @@ userRouter.post("/register", async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
+
+//Login Route
+userRouter.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401).json({ message: "Invalid email or password" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//generate token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
